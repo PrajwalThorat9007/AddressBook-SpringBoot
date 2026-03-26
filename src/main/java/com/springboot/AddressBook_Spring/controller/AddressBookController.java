c96a5ab88d4adbd73f243662df4d92782102502b
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-26 12:20:10 +0530

    [Prajwal]:UC2:rest controller with HTTP methods

---
+++ src/main/java/com/springboot/AddressBook_Spring/controller/AddressBookController.java
@@ -0,0 +1,58 @@
+package com.springboot.AddressBook_Spring.controller;
+
+import com.springboot.AddressBook_Spring.model.AddressBook;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+    List<AddressBook> list = new ArrayList<>();
+
+    @GetMapping
+    public ResponseEntity<List<AddressBook>> getAll() {
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBook> getById(@PathVariable int id){
+        for (AddressBook a : list) {
+            if (a.getId() == id) {
+                return ResponseEntity.ok(a);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressBook> add(@RequestBody AddressBook address){
+        list.add(address);
+        return ResponseEntity.ok(address);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBook> update(@PathVariable int id,@RequestBody AddressBook address){
+        for (AddressBook a : list) {
+
+            if (a.getId() == id) {
+                a.setName(address.getName());
+                a.setCity(address.getCity());
+                return ResponseEntity.ok(a);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id){
+
+        list.removeIf(a -> a.getId() == id);
+        return ResponseEntity.ok("Deleted");
+    }
+
+}
---
+++ src/main/java/com/springboot/AddressBook_Spring/model/AddressBook.java
@@ -0,0 +1,41 @@
+package com.springboot.AddressBook_Spring.model;
+
+public class AddressBook {
+
+
+    private int id;
+    private String name;
+    private String city;
+
+
+    public AddressBook(){}
+
+    public void AddressBook(int id,String name,String city){
+        this.id=id;
+        this.name=name;
+        this.city=city;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
